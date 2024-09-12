46e8d3ba0cb85bcd6c56d3210fd250c355f78b58
mikungg <mikhael.sappy@gmail.com>
2024-09-12 15:23:53 +0700

    struktur folder & routes

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman blog</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+
+    <h1>Halaman Blog</h1>
+
+    <h3>Ini Artikel 1</h3>
+    <article>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias sapiente odio iusto minus quasi atque, itaque odit! Officia, voluptatum commodi nostrum, tempore accusantium, recusandae voluptate cumque repellendus beatae sint corrupti.
+    </article>
+
+    <h3>Ini Artikel 2</h3>
+    <article>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias sapiente odio iusto minus quasi atque, itaque odit! Officia, voluptatum commodi nostrum, tempore accusantium, recusandae voluptate cumque repellendus beatae sint corrupti.
+    </article>
+</body>
+
+</html>
