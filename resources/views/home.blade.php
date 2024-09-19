8524bfbb71960faa0dbb766f04753af495c9a386
mikungg <mikhael.sappy@gmail.com>
2024-09-19 14:21:03 +0700

    Install Tailwind on project + Added navbar on about page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>Home</title>
-    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -13,7 +13,7 @@
     <a href="/about">About</a>
     <a href="/blog">Blog</a>
     <a href="/contact">Contact</a>
-    
+
     <h1>Ini home</h1>
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
 Route::get('/about', function () {
     return view('about', ['nama' => 'Mikha']);
 });
