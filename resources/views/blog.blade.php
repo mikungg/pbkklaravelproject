6bfab82b6170af877041fab37ccd88cf43f19074
mikungg <mikhael.sappy@gmail.com>
2024-09-26 09:08:15 +0700

    Add Layouting component + Navlink component

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,35 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    <title>Halaman blog</title>
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-</head>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
 
-<body>
-    <div class="min-h-full">
-        <x-navbar></x-navbar>
-        
-        <x-header>Blog</x-header>
-  
-        <main>
-          <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h3>Ini Artikel 1</h3>
-            <article>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias sapiente odio iusto minus quasi atque, itaque odit! Officia, voluptatum commodi nostrum, tempore accusantium, recusandae voluptate cumque repellendus beatae sint corrupti.
-            </article>
+    <h2>Ini Artikel 1</h2>
+    <article>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias sapiente odio iusto minus quasi atque, itaque odit! Officia, voluptatum commodi nostrum, tempore accusantium, recusandae voluptate cumque repellendus beatae sint corrupti.
+    </article>
 
-            <h3>Ini Artikel 2</h3>
-            <article>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias sapiente odio iusto minus quasi atque, itaque odit! Officia, voluptatum commodi nostrum, tempore accusantium, recusandae voluptate cumque repellendus beatae sint corrupti.
-            </article>
-          </div>
-        </main>
-    </div>
-</body>
-
-</html>
+    <h2>Ini Artikel 2</h2>
+    <article>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias sapiente odio iusto minus quasi atque, itaque odit! Officia, voluptatum commodi nostrum, tempore accusantium, recusandae voluptate cumque repellendus beatae sint corrupti.
+    </article>
+</x-layout>
